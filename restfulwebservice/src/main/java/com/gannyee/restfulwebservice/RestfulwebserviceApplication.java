2cafb47da69a746f47c36d918369c8bcf0940977
gannyee <gannyee@users.noreply.github.com>
2018-02-06 21:14:55 +0800

    提交demo RESTful-WEB

---
+++ restfulwebservice/src/main/java/com/gannyee/restfulwebservice/RestfulwebserviceApplication.java
@@ -0,0 +1,15 @@
+package com.gannyee.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ∆Ù∂Ø¿‡
+ */
+@SpringBootApplication
+public class RestfulwebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulwebserviceApplication.class, args);
+    }
+}
