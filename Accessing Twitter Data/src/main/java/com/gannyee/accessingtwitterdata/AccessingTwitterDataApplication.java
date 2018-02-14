4028f07aac97f2f74f259357882d88e6c20dbbc9
ganyiwelldone@gmail.com <gy542189475>
2018-02-14 17:19:37 +0800

    提交代码 Accessing data with MySQL

---
+++ Accessing Twitter Data/src/main/java/com/gannyee/accessingtwitterdata/AccessingTwitterDataApplication.java
@@ -0,0 +1,12 @@
+package com.gannyee.accessingtwitterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingTwitterDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingTwitterDataApplication.class, args);
+    }
+}
