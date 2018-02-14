52c8c3996ecee89539d3a71464f876a1b92e236f
ganyiwelldone@gmail.com <gy542189475>
2018-02-14 16:28:57 +0800

    提交代码 定时任务demo

---
+++ Scheduling Tasks/src/main/java/com/gannyee/schedulingytasks/SchedulingTasksApplication.java
@@ -0,0 +1,12 @@
+package com.gannyee.schedulingytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingTasksApplication.class, args);
+    }
+}
