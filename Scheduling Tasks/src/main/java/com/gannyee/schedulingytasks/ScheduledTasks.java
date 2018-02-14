23f2e1f7d42e30eaabc7313c658a6283103e0852
ganyiwelldone@gmail.com <gy542189475>
2018-02-14 16:40:17 +0800

    提交代码 定时任务

---
+++ Scheduling Tasks/src/main/java/com/gannyee/schedulingytasks/ScheduledTasks.java
@@ -0,0 +1,24 @@
+package com.gannyee.schedulingytasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFornate = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        logger.info("The time is now {} ", dateFornate.format(new Date()));
+
+    }
+
+
+}
