74eae5ff85dcd72ea31d3772f5e08a52929b29a6
ganyiwelldone@gmail.com <gy542189475>
2018-08-11 16:24:06 +0800

    更新文件

---
+++ Spring-boot-Swagger2/src/main/java/com/gannyee/swagger2/User.java
@@ -0,0 +1,33 @@
+package com.gannyee.swagger2;
+
+public class User {
+
+
+    String name;
+    int age;
+
+    long  id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
